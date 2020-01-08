3c4882cf77351c69806f046288cd7e5d639ba2e8
NEWICT\aiman <aiman@newict.com>
2020-01-08 16:40:03 +0800

    first commit

---
+++ MQTT_Test/MainWindow.xaml.cs
@@ -0,0 +1,356 @@
+﻿using MQTTnet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows.Threading;
+using MQTTnet.Server;
+using MQTTnet.Protocol;
+using MQTTnet.Client.Disconnecting;
+
+namespace MQTT_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IMqttServer mqttServer;
+
+        MqttServerOptionsBuilder optionsBuilder = new MqttServerOptionsBuilder();
+
+        List<string> clientSession = new List<string>();
+
+        MqttServerConnectionValidatorDelegate connValidator = new MqttServerConnectionValidatorDelegate(c =>
+        {
+            if (c.ClientId.Length < 10)
+            {
+                c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedIdentifierRejected;
+                return;
+            }
+
+            if (c.Username != "mySecretUser")
+            {
+                c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                return;
+            }
+
+            if (c.Password != "mySecretPassword")
+            {
+                c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                return;
+            }
+
+            c.ReturnCode = MqttConnectReturnCode.ConnectionAccepted;
+        });
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Configure MQTT server.
+            //mqttServer = new MqttFactory().CreateMqttServer(options =>
+            //{
+            //    options.DefaultEndpointOptions.Port = 1884;
+            //    //options.Storage = new RetainedMessageHandler();
+            //    options.ConnectionValidator = c =>
+            //    {
+            //        if (c.ClientId.Length < 10)
+            //        {
+            //            return MqttConnectReturnCode.ConnectionRefusedIdentifierRejected;
+            //        }
+
+            //        //if (c.Username != "mySecretUser")
+            //        //{
+            //        //    return MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+            //        //}
+
+            //        //if (c.Password != "mySecretPassword")
+            //        //{
+            //        //    return MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+            //        //}
+
+            //        return MqttConnectReturnCode.ConnectionAccepted;
+            //    };
+            //});            
+
+            optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionBacklog(100)
+                //.WithConnectionValidator(connValidator)
+                .WithDefaultEndpointPort(60000);           
+
+            mqttServer = new MqttFactory().CreateMqttServer();
+
+            mqttServer.StartAsync(optionsBuilder.Build());
+         
+            mqttServer.UseApplicationMessageReceivedHandler(async e =>
+            {
+                string receivedMqtt = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                if (e.ApplicationMessage.Topic.StartsWith("Senzo/Mobile/Client/"))
+                {
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE FROM MOBILE ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+                    await Application.Current.Dispatcher.BeginInvoke(
+                             DispatcherPriority.Background,
+                             new Action(() =>
+                             {
+                                 txtMessage.Text = "";
+                                 txtMessage.Text += "### RECEIVED APPLICATION MESSAGE ###";
+                                 txtMessage.Text += "$ Topic = " + e.ApplicationMessage.Topic;
+                                 txtMessage.Text += "$ Payload = " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                                 txtMessage.Text += "$ QoS = " + e.ApplicationMessage.QualityOfServiceLevel;
+                                 txtMessage.Text += "$ Retain = " + e.ApplicationMessage.Retain;
+                             })
+                    );
+
+                    var message2 = new MqttApplicationMessageBuilder();
+                    //.WithTopic("Senzo/Server/+")
+                    //.WithPayload("Receive And Forward Message")
+                    //.WithExactlyOnceQoS()
+                    //.WithRetainFlag()
+                    //.Build();
+                    //foreach (string clientId in clientSession) {
+                    //    await mqttServer.PublishAsync(message2.WithTopic("Senzo/Gateway/Server/" +clientId)
+                    //        .WithPayload(receivedMqtt)
+                    //        .WithExactlyOnceQoS()
+                    //        .WithRetainFlag()
+                    //        .Build());
+                    //}
+                    //Console.WriteLine();
+                }
+                else if (e.ApplicationMessage.Topic.StartsWith("Senzo/Gateway/Client/"))
+                {
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE FROM GATEWAY ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+                    await Application.Current.Dispatcher.BeginInvoke(
+                             DispatcherPriority.Background,
+                             new Action(() =>
+                             {
+                                 txtMessage.Text = "";
+                                 txtMessage.Text += "### RECEIVED APPLICATION MESSAGE ###";
+                                 txtMessage.Text += "$ Topic = " + e.ApplicationMessage.Topic;
+                                 txtMessage.Text += "$ Payload = " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                                 txtMessage.Text += "$ QoS = " + e.ApplicationMessage.QualityOfServiceLevel;
+                                 txtMessage.Text += "$ Retain = " + e.ApplicationMessage.Retain;
+                             })
+                    );
+
+
+
+                    var message = new MqttApplicationMessageBuilder();
+                    //.WithTopic("Senzo/Server/+")
+                    //.WithPayload("Receive And Forward Message")
+                    //.WithExactlyOnceQoS()
+                    //.WithRetainFlag()
+                    //.Build();
+
+                    //foreach (string clientId in clientSession)
+                    //{
+                    //    await mqttServer.PublishAsync(message.WithTopic("Senzo/Gateway/Server/" + clientId)
+                    //        .WithPayload(receivedMqtt)
+                    //        .WithExactlyOnceQoS()
+                    //        .WithRetainFlag()
+                    //        .Build());
+                    //}
+
+                    //Console.WriteLine();
+                }
+                else if ((string.Equals(e.ApplicationMessage.Topic, "test/topic")))
+                {
+                    Console.WriteLine("### IGNORED APPLICATION MESSAGE FROM GATEWAY ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+
+                   
+                } else if (e.ApplicationMessage.Topic.StartsWith("MQTTnet.RPC") && e.ApplicationMessage.Topic.EndsWith("/Reply") && e.ClientId !=null) {
+                    var message1 = new MqttApplicationMessageBuilder();
+                    
+                    Console.WriteLine("### RPC APPLICATION MESSAGE FROM GATEWAY ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine($"+ ContentType = {e.ApplicationMessage.ContentType}");
+                    Console.WriteLine();
+                  
+                    await mqttServer.PublishAsync(message1.WithTopic(e.ApplicationMessage.Topic+ "/response")
+                                .WithPayload("TestResponse")
+                                .WithExactlyOnceQoS()
+                                .Build());
+                }
+                else {
+                    Console.WriteLine("### IGNORED APPLICATION MESSAGE FROM GATEWAY ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+
+
+                }
+
+            });
+
+            mqttServer.ClientDisconnectedHandler = new MqttServerClientDisconnectedHandlerDelegate(e => OnDisconnected(e));
+
+            mqttServer.ClientConnectedHandler = new MqttServerClientConnectedHandlerDelegate(e => OnConnected(e));
+
+            //mqttServer.ApplicationMessageReceivedHandler = new Mqtt(e => OnAppMessage(e));
+
+            //mqttServer.UseClientConnectedHandler(e =>
+            //    {
+            //        Console.WriteLine("Client Connected:"+e.ClientId);
+            //        clientSession.Add(e.ClientId);
+            //    });
+
+            //mqttServer.UseClientDisconnectedHandler(e =>
+            //{
+            //    Console.WriteLine("Client Disconnected:"+e.ClientId);
+            //    clientSession.Remove(e.ClientId);
+            //});
+        }
+
+        private void OnConnected(MqttServerClientConnectedEventArgs e)
+        {
+            Console.WriteLine("Client Connected:" + e.ClientId);
+            clientSession.Add(e.ClientId);
+        }
+
+        private void OnDisconnected(MqttServerClientDisconnectedEventArgs e)
+        {
+            Console.WriteLine("Client Disconnected:" + e.ClientId);
+            clientSession.Remove(e.ClientId);
+        }
+
+        private void OnDisconnected(MqttClientDisconnectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //MqttServerApplicationMessageInterceptorDelegate asd = new MqttServerApplicationMessageInterceptorDelegate(e =>
+        //  {
+        //      if (e.ApplicationMessage.Topic == "message")
+        //      {
+        //          Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+        //          Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+        //          Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+        //          Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+        //          Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+        //          Console.WriteLine();
+        //          Application.Current.Dispatcher.BeginInvoke(
+        //                   DispatcherPriority.Background,
+        //                   new Action(() =>
+        //                   {
+        //                       txtMessage.Text = "";
+        //                       txtMessage.Text += "### RECEIVED APPLICATION MESSAGE ###\n";
+        //                       txtMessage.Text += "$ Topic = " + e.ApplicationMessage.Topic + "\n";
+        //                       txtMessage.Text += "$ Payload = " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload) + "\n";
+        //                       txtMessage.Text += "$ QoS = " + e.ApplicationMessage.QualityOfServiceLevel + "\n";
+        //                       txtMessage.Text += "$ Retain = " + e.ApplicationMessage.Retain + "\n";
+        //                   })
+        //          );
+        //          //Console.WriteLine();
+        //      }
+        //  } );
+        private void MqttServer_ClientConnected(object sender, MqttServerClientConnectedEventArgs e)
+        {
+            Console.WriteLine(e.ClientId);
+        }
+
+        private void MqttServer_ApplicationMessageReceived(object s, MqttApplicationMessageReceivedEventArgs e)
+        {
+            if(e.ApplicationMessage.Topic == "message")
+            {
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+                Application.Current.Dispatcher.BeginInvoke(
+                         DispatcherPriority.Background,
+                         new Action(() =>
+                         {
+                             txtMessage.Text = "";
+                             txtMessage.Text += "### RECEIVED APPLICATION MESSAGE ###\n";
+                             txtMessage.Text += "$ Topic = " + e.ApplicationMessage.Topic + "\n";
+                             txtMessage.Text += "$ Payload = " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload) + "\n";
+                             txtMessage.Text += "$ QoS = " + e.ApplicationMessage.QualityOfServiceLevel + "\n";
+                             txtMessage.Text += "$ Retain = " + e.ApplicationMessage.Retain + "\n";
+                         })
+                );
+                //Console.WriteLine();
+            }
+        }
+
+        public async void StartServer()
+        {
+            optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionBacklog(100)
+                //.WithConnectionValidator(connValidator)
+                .WithDefaultEndpointPort(1884);
+            await mqttServer.StartAsync(optionsBuilder.Build());
+        }
+
+        public async void StopServer()
+        {
+            await mqttServer.StopAsync();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StartServer();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            StopServer();
+        }
+    }
+
+
+    // The implementation of the storage:
+    // This code uses the JSON library "Newtonsoft.Json".
+    public class RetainedMessageHandler : IMqttServerStorage
+    {
+        private const string Filename = "D:\\RetainedMessages.json";
+
+        public Task SaveRetainedMessagesAsync(IList<MqttApplicationMessage> messages)
+        {
+            File.WriteAllText(Filename, JsonConvert.SerializeObject(messages));
+            return Task.FromResult(0);
+        }
+
+        public Task<IList<MqttApplicationMessage>> LoadRetainedMessagesAsync()
+        {
+            IList<MqttApplicationMessage> retainedMessages;
+            if (File.Exists(Filename))
+            {
+                var json = File.ReadAllText(Filename);
+                retainedMessages = JsonConvert.DeserializeObject<List<MqttApplicationMessage>>(json);
+            }
+            else
+            {
+                retainedMessages = new List<MqttApplicationMessage>();
+            }
+
+            return Task.FromResult(retainedMessages);
+        }
+        
+    }
+
+}
---
+++ MQTT_TestClient_UI/MainWindow.xaml.cs
@@ -0,0 +1,253 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Extensions.Rpc;
+using MQTTnet.Protocol;
+using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MQTT_TestClient_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string clientId;
+
+        int retryConnect = 0;
+
+        IMqttClient mqttClient;
+
+        MqttRpcClient rpcClient;
+
+        TimeSpan timeout = TimeSpan.FromSeconds(5);
+
+        MqttQualityOfServiceLevel qos = MqttQualityOfServiceLevel.AtMostOnce;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Create a new MQTT client.
+            var factory = new MqttFactory();
+            mqttClient = factory.CreateMqttClient();
+
+            clientId = Guid.NewGuid().ToString();
+
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("IsConnected:" + mqttClient.IsConnected);
+                retryConnect = 0;
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                var message = new MqttApplicationMessageBuilder()
+                           .WithTopic("test/topic")
+                           .WithPayload("test")
+                           .WithExactlyOnceQoS()
+                           //.WithRetainFlag()
+                           .Build();
+
+                //for (int i= 0; i < 50; i++) {
+                //await mqttClient.PublishAsync(message);
+                // Subscribe to a topic
+               await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic(" anonymous/Server").Build());
+
+                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("GatewayIP/#").Build());
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+
+            //mqttClient.UnsubscribeAsync();
+
+            //mqttClient.UseApplicationMessageReceivedHandler(async e =>
+            //{
+            //    if (e.ApplicationMessage.Topic == "/anonymous/Server/ClientMob")
+            //    {
+            //        Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+            //        Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+            //        Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            //        Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+            //        Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+            //        Console.WriteLine();
+            //        await Application.Current.Dispatcher.BeginInvoke(
+            //                 DispatcherPriority.Background,
+            //                 new Action(() =>
+            //                 {
+            //                     txtMessage.Text = "";
+            //                     txtMessage.Text += "### RECEIVED APPLICATION MESSAGE ###";
+            //                     txtMessage.Text += "$ Topic = " + e.ApplicationMessage.Topic;
+            //                     txtMessage.Text += "$ Payload = " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            //                     txtMessage.Text += "$ QoS = " + e.ApplicationMessage.QualityOfServiceLevel;
+            //                     txtMessage.Text += "$ Retain = " + e.ApplicationMessage.Retain;
+            //                 })
+            //        );
+            //        //Console.WriteLine();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("### IGNORED APPLICATION MESSAGE ###");
+            //        Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+            //        Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            //        Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+            //        Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+            //        Console.WriteLine();
+            //    }
+            //});
+
+            mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(async e =>
+            {
+                Console.WriteLine("IsConnected:"+mqttClient.IsConnected);
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine("MQTT User "+clientId+" received from:" + e.ClientId);
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+               // await mqttClient.PublishAsync(e.ApplicationMessage.Topic + "/response", "pong");
+            });
+        }
+
+        //void mqtt_Callback(string topic, string payload, int payloadLength)
+        //{
+        //    string topicString = topic;
+
+        //    if (topicString.StartsWith("MQTTnet.RPC/"))
+        //    {
+        //        string responseTopic = topicString + ("/response");
+
+        //        if (topicString.EndsWith("/deviceA.ping"))
+        //        {
+        //            mqtt_publish(responseTopic, "pong", false);
+        //            return;
+        //        }
+        //    }
+        //}
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            // Use WebSocket connection.
+            var options = new MqttClientOptionsBuilder()
+                //.WithWebSocketServer("192.168.1.106:1883/mqtt") 
+                .WithClientId("123")
+                .WithTcpServer("localhost", 60000)
+                .WithTls(new MqttClientOptionsBuilderTlsParameters
+                {
+                    CertificateValidationCallback = (X509Certificate x, X509Chain y, SslPolicyErrors z, IMqttClientOptions o) =>
+                    {
+                        // TODO: Check conditions of certificate by using above parameters.
+                        return true;
+                    }
+                })
+                .Build();
+
+            mqttClient.UseDisconnectedHandler(async c =>
+            {
+                Console.WriteLine("### DISCONNECTED FROM SERVER ###");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                try
+                {
+                    //if (retryConnect > 3)
+                    //{
+                    //    await mqttClient.DisconnectAsync();
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("IsConnected:" + mqttClient.IsConnected);
+                    //    //if (retryConnect > 3)
+                    //    //{
+                    //    //    Console.WriteLine("Exiting Program");
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //    Console.WriteLine("Retry");
+                    //    await mqttClient.ConnectAsync(options);
+                    //    //}
+                    //    //retryConnect++;
+                    //    //Console.WriteLine("asda:"+c.Exception.ToString());
+                    //}
+                }
+                catch
+                {
+                    Console.WriteLine("### RECONNECTING FAILED ###");
+                    retryConnect++;
+                    //Console.WriteLine("asda:" + c.Exception.ToString());
+                }
+            });
+
+            try
+            {
+                await mqttClient.ConnectAsync(options);
+
+                //await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("MQTTnet.RPC/+/Reply").Build());
+
+                rpcClient = new MqttRpcClient(mqttClient);
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error When Connecting:" + ex);
+            }
+            
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var message = new MqttApplicationMessageBuilder()
+                                .WithTopic(txtTopic.Text)
+                                .WithPayload(txtMessage.Text)
+                                .WithExactlyOnceQoS()
+                                //.WithRetainFlag()
+                                .Build();
+
+                //for (int i= 0; i < 50; i++) {
+                //await mqttClient.PublishAsync(message);
+
+                var response = await rpcClient.ExecuteAsync(timeout, "Reply", "testRPC", qos);
+
+                Console.WriteLine("RPC Response: "+Encoding.UTF8.GetString(response));
+
+
+                //await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("MQTTnet.RPC/+/Response").Build());
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error When Sending RPC:" + ex);            }
+            //}
+
+            
+        }
+
+        protected virtual void OnAppMessage(MqttApplicationMessageReceivedEventArgs e)
+        {
+            if (e.ApplicationMessage.Topic == "/anonymous/Server")
+            {
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+                Application.Current.Dispatcher.BeginInvoke(
+                         DispatcherPriority.Background,
+                         new Action(() =>
+                         {
+                             txtMessage.Text = "";
+                             txtMessage.Text += "### RECEIVED APPLICATION MESSAGE ###";
+                             txtMessage.Text += "$ Topic = " + e.ApplicationMessage.Topic;
+                             txtMessage.Text += "$ Payload = " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                             txtMessage.Text += "$ QoS = " + e.ApplicationMessage.QualityOfServiceLevel;
+                             txtMessage.Text += "$ Retain = " + e.ApplicationMessage.Retain;
+                         })
+                );
+                //Console.WriteLine();
+            }
+        }
+    }
+}
